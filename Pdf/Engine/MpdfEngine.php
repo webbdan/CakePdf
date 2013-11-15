c9b99b97d2110bfd5e13175b7f40490bec7700ea
Dan Webb <dan.webb@ldnlpt1281.bjss.co.uk>
2013-11-15 13:32:29 +0000

    change mpdf to use pageSize config setting and proper margins

--- Pdf/Engine/MpdfEngine.php
+++ Pdf/Engine/MpdfEngine.php
@@ -22,7 +22,7 @@ class MpdfEngine extends AbstractPdfEngine {
 	public function output() {
 		//mPDF often produces a whole bunch of errors, although there is a pdf created when debug = 0
 		//Configure::write('debug', 0);
-		$MPDF = new mPDF();
+		$MPDF=new mPDF('',$this->_Pdf->pageSize(), 0, '', 15, 15, 20, 32, 9, 9);
 		$MPDF->writeHTML($this->_Pdf->html());
 		return $MPDF->Output('', 'S');
 	}
